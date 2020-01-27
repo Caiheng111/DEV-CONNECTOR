c79ad41d01ab2fd387f07708145f3412085613e4
Cai heng <ianyaheng822@gmail.com>
2020-01-27 18:02:49 +1100

    add the auth action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 // import axios from 'axios'
 import {connect} from 'react-redux'
 import {setAlert} from '../../actions/alert'
+import {register} from '../../actions/auth'
 import PropTypes from 'prop-types'
 
 
@@ -29,20 +30,7 @@ import PropTypes from 'prop-types'
       
       } else {
         console.log('SUCCESS');
-        // const newUser={name, email, password}
-        // try{
-        //   const config = {
-        //     headers:{
-        //       'content-type':"application/json"
-        //     } 
-        //   }
-        //   const body=JSON.stringify(newUser)
-        //   const res= await axios.post('/api/users', body, config)
-        //   console.log(res.data);
-        // } catch(err){
-        //   console.error(err.response.data);
-          
-        // }
+        props.register({name, email,password})
         
       }
     };
@@ -107,7 +95,8 @@ import PropTypes from 'prop-types'
 
 Register.protoTypes = {
   setAlert :PropTypes.func.isRequired,
+  register :PropTypes.func.isRequired,
 }
 
 
-export default connect(null, {setAlert})(Register)
+export default connect(null, {setAlert, register})(Register)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 
 import {combineReducers} from 'redux'
 import alert from '../reducers/alert'
+import auth from '../reducers/auth'
+
 
 export default combineReducers({
-  alert
+  alert,
+  auth
 });
