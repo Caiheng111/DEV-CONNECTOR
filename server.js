87db3c7a6e92d59a024a95a98c2a0b699ce837ca
Cai heng <ianyaheng822@gmail.com>
2020-02-09 15:51:05 +1100

    fix the conflict

--- server.js
+++ server.js
@@ -1,39 +1,31 @@
-const express=require('express');
-const connectDB=require('./config/db');
-const app=express();
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const path= require('path')
+const app = express();
 
+// Connect Database
 connectDB();
+
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-const userRouter=require('./routes/api/users');
-const profileRouter=require('./routes/api/profile');
-const postsRouter=require('./routes/api/posts');
-const authRouter=require('./routes/api/auth');
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-// app.get('/',(req,res)=>{
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-//   res.send('API running')
-// })
-
-
-//serve static aessect in production
-
-if(process.env.NODE_ENV=== 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
+const PORT = process.env.PORT || 5000;
 
-//define routes
-app.use('/api/users',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/profile',profileRouter);
-app.use('/api/posts',postsRouter);
-
-const PORT=process.env.PORT || 5000;
-
-app.listen(PORT,console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
